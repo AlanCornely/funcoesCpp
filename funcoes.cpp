4c291f4e1b309c79a1a08a67a1145e27e51b5535
AlanCornely <162385514+AlanCornely@users.noreply.github.com>
2024-05-01 00:37:37 +0000

    pronto

--- funcoes.cpp
+++ funcoes.cpp
@@ -10,8 +10,22 @@ int soma (int a, int b){
     return a + b;
 }
 //----------------------------------------------
-//declarar função
+//declarar função que retorna par
 bool ehPar (int num){
     return num % 2 == 0;
+}
+//----------------------------------------------
+//declarar função para fatorial
+int fatorial(int num){
+    int resultado = 1;
+    for (int i= 2; i <= num; ++i){
+        resultado *= i;
+    }
+    return resultado;
+}
+//----------------------------------------------
+//declarar função de converção de temperatura
+double cParaf( double celsius){
+    return (celsius * 9 / 5) + 32;
 }
 //----------------------------------------------
